ad54f52ff180d740101b598848609300b779ccaf
Osman Shakib <osmanshakib@gmail.com>
2021-12-07 00:56:51 +0000

    shortening urls

--- express_server.js
+++ express_server.js
@@ -12,17 +12,34 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   });
+  
   app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
   });
+  
   app.get("/urls", (req, res) => {
     const templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
   });
+  
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+  
+  app.get("/urls/:shortURL", (req, res) => {
+    const shortURL = req.params.shortURL;
+    let templateVars = { shortURL, longURL: urlDatabase[shortURL]};
+    res.render("urls_show", templateVars);
+  });
+  
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
 
-app.listen(PORT, () => {
+  app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-});
+}); 
