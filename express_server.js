47c9e8d17d4939f0124d317e3e548a5a44f10463
Osman Shakib <osmanshakib@gmail.com>
2021-12-09 22:56:33 +0000

    resgistering users

--- express_server.js
+++ express_server.js
@@ -2,7 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-
+ const users = { 
+     "userRandomID": {
+       id: "userRandomID", 
+       email: "user@example.com", 
+       password: "purple-monkey-dinosaur"
+     },
+    "user2RandomID": {
+       id: "user2RandomID", 
+       email: "user2@example.com", 
+       password: "dishwasher-funk"
+     }
+   }
+  
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -10,6 +22,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser())
 app.set("view engine", "ejs");
 
+const getUserEmail = (user_id) => {
+    for (const T in users ) {
+     console.log((`${T}: ${users[T].email}`)); 
+     if (user_id === T){
+         return users[T].email 
+        
+     } else {
+         return false
+     }
+    }
+} 
 function generateRandomString() {
   let result           = '';
   let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -20,8 +43,8 @@ function generateRandomString() {
   return result;
 }
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": "http:www.lighthouselabs.ca",
+  "9sm5xK": "http:www.google.com"
 };
 
 app.get("/", (req, res) => {
@@ -37,47 +60,68 @@ app.get("/hello", (req, res) => {
 });
   
 app.get("/urls", (req, res) => {
+    console.log(req.cookies.user_id)
+    let user = getUserEmail(req.cookies.user_id)
   const templateVars = { 
       urls: urlDatabase,
-     user: req.cookies.username,
+     user ,
     };
   res.render("urls_index", templateVars);
 });
   
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+
+app.get("/register", (req, res) => {
+    res.render("register");
 });
 
 app.get("/u/:shortURL", (req, res) => {
     const longURL = urlDatabase[req.params.shortURL];
-   res.redirect(longURL);
-  });
+    res.redirect(longURL);
+});
 
 app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`urls/${shortURL}`);
+    const shortURL = generateRandomString();
+    urlDatabase[shortURL] = req.body.longURL;
+    res.redirect(`urls/${shortURL}`);
 });
-  
+
+app.get("/urls/new", (req, res) => {
+    const templateVars = {
+        user: {}
+    };
+  res.render("urls_new", templateVars);
+});
+
 app.post("/urls/:shortURL", (req, res) => {
     let shortURL = req.params.shortURL;
     urlDatabase[shortURL] = req.body.longURL
     res.redirect("/urls")
 })
 
+
+app.post("/register", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const id = generateRandomString()
+    users[id] = {
+        password, email, id
+    }
+    res.cookie("user_id", id)
+    res.redirect('/urls');
+    console.log(users)
+})
+
 app.post("/login", (req, res) => {
     const username = req.body.Username
     res.cookie("username", username);
-    res.redirect("/urls")
+    res.redirect('/urls')
 })
-
 app.post("/logout", (req, res) => {
     res.clearCookie("username");
     res.redirect('/urls');
   })
 
-
-app.post("/urls/:shortURL/delete", (req, res) => {
+  app.post("/urls/:shortURL/delete", (req, res) => {
 let shortURL = req.params.shortURL;
 delete urlDatabase[shortURL]
 res.redirect("/urls")
@@ -86,13 +130,13 @@ res.redirect("/urls")
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  let templateVars = { shortURL, longURL: urlDatabase[shortURL]};
+  let templateVars = { shortURL, longURL: urlDatabase[shortURL],user:""};
   res.render("urls_show", templateVars);
 });
   
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
+ app.get("/urls/new", (req, res) => {
+   res.render("urls_new");
+ });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
